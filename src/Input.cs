d8b9a57a42ac265182a5de31deb46f9e65ee84f5
oliolden <oliver@oldenstam.se>
2022-05-20 12:40:17 +0200

    Added Input class.

--- src/Input.cs
+++ src/Input.cs
@@ -1,9 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Microsoft.Xna.Framework.Input;
 
 namespace CyberCity {
     internal static class Input {
-        static bool left = false, right = false;
+        public static Dictionary<string, Keys> binds = new Dictionary<string, Keys> {
+            { "left", Keys.A },
+            { "right", Keys.D },
+            { "sprint", Keys.LeftShift },
+            { "jump", Keys.Space },
+            { "attack", Keys.E },
+        };
+        static bool left = false, right = false, sprint = false;
+        public static event EventHandler Jump;
+        public static event EventHandler Attack;
+
+        private static KeyboardState keyboard;
+
+        public static void Update() {
+            KeyboardState prevKeyboard = keyboard;
+            keyboard = Keyboard.GetState();
+            if (keyboard.IsKeyDown(binds["left"])) left = true;
+            else left = false;
+            if (keyboard.IsKeyDown(binds["right"])) right = true;
+            else right = false;
+            if (keyboard.IsKeyDown(binds["sprint"])) sprint = true;
+            else sprint = false;
+            if (keyboard.IsKeyDown(binds["jump"]) && prevKeyboard.IsKeyUp(binds["jump"])) Jump?.Invoke(null, null);
+            if (keyboard.IsKeyDown(binds["attack"]) && prevKeyboard.IsKeyUp(binds["attack"])) Attack?.Invoke(null, null);
+        }
     }
 }
